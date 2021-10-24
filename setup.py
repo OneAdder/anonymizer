82025fc16e7afaa58618fac1429c214f86ec1199
OneAdder <mikivo@list.ru>
2021-10-24 20:26:44 +0300

    обновил сетуп

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ import setuptools
 
 with open("requirements.txt") as fp:
     install_requires = fp.read()
+with open("long_description.md") as long_desc_file:
+    long_desc = long_desc_file.read()
 
 
 setuptools.setup(
     name="anonymizer",
-    version="0.1",
-    author="Emelyanov Yaroslav",
+    version="0.1rc1",
+    author="Deep People",
     description="Anonymizer service",
-    long_description="Anonymizer service",
+    long_description=long_desc,
     long_description_conttype="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
+    url="https://github.com/OneAdder/anonymizer",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
     ],
     install_requires=install_requires,
     zip_safe=False,
