2f54ddb7e3eaf22b3b772366ec0cfb2a692cd107
OneAdder <mikivo@list.ru>
2021-10-24 14:59:04 +0300

    добавил скрипт с установкой всех дебиановских зависимостей

---
+++ debian10-deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt install zlib1g libjpeg-dev pkg-config libhdf5-103 libhdf5-dev poppler-utils tesseract-ocr tesseract-ocr-rus libreoffice python3-pip gunicorn3
+pip3 install unoserver
