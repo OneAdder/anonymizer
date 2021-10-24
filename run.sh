30c08c5762eabad31538d7b885962e41d2b4092a
OneAdder <mikivo@list.ru>
2021-10-24 15:45:42 +0300

    расширил скрипт запуска

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+echo "Starting unoserver"
 unoserver &
-venv/bin/gunicorn -w1 -t180 -b 127.0.0.1:8080 anonymizer.app:app --access-logfile access-log
+if [ -z "$1" ]; then
+  listen=127.0.0.1:8080
+else
+  listen="$1"
+fi
+echo "Starting webapp on $1"
+venv/bin/gunicorn -w1 -t180 -b "$listen" anonymizer.app:app --access-logfile access-log
+echo "Stopping webapp"
+pkill gunicorn
+echo "Stopping unoserver"
+killall unoserver
